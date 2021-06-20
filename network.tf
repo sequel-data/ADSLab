50163bcbba326995af524cd54be8009d03022301
rsmithtamu <rsmith@netandvet.com>
2021-06-19 19:53:52 -0500

    Update network.tf

--- network.tf
+++ network.tf
@@ -27,36 +27,17 @@ resource "azurerm_public_ip" "domain-controller" {
   idle_timeout_in_minutes = 30
 
 }
-resource "azurerm_public_ip" "domain-controller-2" {
-  name                    = "DC-2"  
-  location            = azurerm_resource_group.resourcegroup.location
-  resource_group_name = azurerm_resource_group.resourcegroup.name
-  allocation_method       = "Dynamic"
-  idle_timeout_in_minutes = 30
-
-}
 
 # Network interface for the DC
-resource "azurerm_network_interface" "domain-controller-2" {
-  name                = "domain-controller-nic-2"
-  location            = azurerm_resource_group.resourcegroup.location
-  resource_group_name = azurerm_resource_group.resourcegroup.name
-  ip_configuration {
-    name                          = "dc-2"resource "azurerm_network_interface" "domain-controller-2" {
+resource "azurerm_network_interface" "domain-controller" {
   name                = "domain-controller-nic"
   location            = azurerm_resource_group.resourcegroup.location
   resource_group_name = azurerm_resource_group.resourcegroup.name
   ip_configuration {
-    name                          = "dc-2"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "static"
-    private_ip_address = "10.0.0.11"
-    public_ip_address_id  = azurerm_public_ip.domain-controller-2.id
-  }
+    name                          = "dc-1"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "static"
     private_ip_address = "10.0.0.10"
     public_ip_address_id  = azurerm_public_ip.domain-controller.id
   }
-    
 }
